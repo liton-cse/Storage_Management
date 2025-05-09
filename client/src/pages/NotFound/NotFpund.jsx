19ac28761eff49a85b979fb1d868e7571ac135b1
liton13 <litonakash13@gmail.com>
2025-05-10 01:28:27 +0600

    fall route

--- client/src/pages/NotFound/NotFpund.jsx
+++ client/src/pages/NotFound/NotFpund.jsx
@@ -1,13 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import "./NotFound.css";
+import { useAuth } from "../../context/AuthContext";
 const NotFoundPage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const handleClick = () => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  };
   return (
     <div className="not-found-area">
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
-      <button onClick={() => navigate("/")} className="not-found-button">
-        Go to Home
+      <button onClick={handleClick} className="not-found-button">
+        {user ? "Go to Home" : "Go To Login"}
       </button>
     </div>
   );
